d38aab5d2571837aab5761f863c3898c22af2aaf
Aditya Singh <123162244+adysinghh@users.noreply.github.com>
2023-11-15 23:25:48 +0530

    home-screen-started
    home-screen-started

--- app/src/main/java/com/example/travelmate/MainActivity.kt
+++ app/src/main/java/com/example/travelmate/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.travelmate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.travelmate.fragments.home_main_fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.white)
+        }
+        val decor = window.decorView
+        decor.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+
+        // Set the initial fragment
+        val initialFragment: Fragment = home_main_fragment()
+        replaceFragment(initialFragment)
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragmentFrame, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/travelmate/fragments/home_main_fragment.java
@@ -0,0 +1,29 @@
+package com.example.travelmate.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.travelmate.R;
+
+
+public class home_main_fragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_main_fragment, container, false);
+
+        return view;
+    }
+}

d68e149a28a75c5ea11c2c1640b89ea8415d74fa
Harshita Shreya <112571825+Harshi-63@users.noreply.github.com>
2023-11-16 22:06:27 +0530

    my trip initial commit

---
+++ app/src/main/java/com/example/travelmate/MyTrip_adapter.kt
@@ -0,0 +1,22 @@
+package com.example.travelmate
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+    class MyTrip_adapter(
+        fragmentManager: FragmentManager,
+        lifecycle: Lifecycle
+    ): FragmentStateAdapter(fragmentManager, lifecycle) {
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return if (position==0)
+                Active_trips()
+            else
+                Passed_trips()
+        }
+    }
---
+++ app/src/main/java/com/example/travelmate/My_trip_record.kt
@@ -0,0 +1,44 @@
+package com.example.travelmate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+
+class My_trip_record:AppCompatActivity(){
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentStateAdapter
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_my_trip_record)
+        tabLayout=findViewById(R.id.trip_record)
+        viewPager2=findViewById(R.id.trip_recordPager)
+        adapter=MyTrip_adapter(supportFragmentManager, lifecycle)
+        tabLayout.addTab(tabLayout.newTab().setText("Active"))
+        tabLayout.addTab(tabLayout.newTab().setText("Passed"))
+        viewPager2.adapter=adapter
+        tabLayout.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem=tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+        viewPager2.registerOnPageChangeCallback(object:ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrollStateChanged(position: Int) {
+                super.onPageScrollStateChanged(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+    }
+}

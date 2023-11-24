fdcfcc4394bda3972803730a816c4a06489b7c6d
Sneha <612.ssneha@gmail.com>
2023-11-24 17:12:39 +0530

    3 fragments xml created

---
+++ app/src/main/java/com/example/travelmate/AllSet.kt
@@ -0,0 +1,11 @@
+package com.example.travelmate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AllSet : Fragment(R.layout.fragment_all_set) {
+
+}

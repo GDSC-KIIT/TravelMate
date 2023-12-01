5fd3d2f82013ef301cdd9dbd4f4cbba1224b8b89
Aditya Singh <123162244+adysinghh@users.noreply.github.com>
2023-12-02 02:26:39 +0530

    added button and scrollView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,5 +50,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.15.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
 
 }

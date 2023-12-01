0449b007262c2c5c7dd5bc62bebb88ae9ab8d788
Maniii97 <mjha34499@gmail.com>
2023-12-02 01:59:09 +0530

    added settings fragment

--- app/src/main/java/com/example/travelmate/fragments/home_main_fragment.java
+++ app/src/main/java/com/example/travelmate/fragments/home_main_fragment.java
@@ -2,17 +2,24 @@ package com.example.travelmate.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.travelmate.Adapters.CategoryAdaptar;
 import com.example.travelmate.R;
 import com.example.travelmate.models.CategoryDomain;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -22,10 +29,16 @@ public class home_main_fragment extends Fragment {
     private RecyclerView popmenu;
     private RecyclerView.Adapter adapter;
 
+    private TextView tvViewAllPopularDest;
+    private TextView tvViewAllArticles;
+
+    private NavController navController;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -33,8 +46,23 @@ public class home_main_fragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_main_fragment, container, false);
 
+
+        tvViewAllPopularDest = view.findViewById(R.id.tvViewAllPopularDestination);
+        tvViewAllArticles = view.findViewById(R.id.tvViewAllArticles);
+
+
         return view;
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController= Navigation.findNavController(view);
+
+    }
+
+
 
     private void recyclerViewCategory(){
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);

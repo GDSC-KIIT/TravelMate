7523fab658934eea6718ad8360af75dc693ae16b
Rhapsodictree <nischaykashyap3@gmail.com>
2024-01-01 16:07:19 +0530

    1jan(navigation updated)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.travelmate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.travelmate"
@@ -42,11 +42,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.android.car.ui:car-ui-lib:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/travelmate/SettingsActivity.kt
+++ app/src/main/java/com/example/travelmate/SettingsActivity.kt
@@ -3,41 +3,22 @@ package com.example.travelmate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.SearchView
-import com.example.travelmate.databinding.FaqBinding
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : FaqBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FaqBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.fragment_settings_main)
 
-
-        val faq = arrayOf("What is TravelMate","Travelmate is a travel planning and exploration platform that helps you discover and plan trips to your dream locations"
-        )
-        val faqAdapter : ArrayAdapter<String> = ArrayAdapter(
-            this,android.R.layout.simple_expandable_list_item_2,
-            faq
-        )
-        binding.listView.adapter = faqAdapter;
-        binding.searchFaq.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                binding.searchFaq.clearFocus()
-                if (faq.contains(query)){
-                    faqAdapter.filter.filter(query)
-                }
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                faqAdapter.filter.filter(newText)
-            return false
-            }
-
-        })
     }
 }
+
